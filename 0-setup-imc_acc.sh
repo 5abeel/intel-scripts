bcda4137ee81668e7df90b45641ef455f2897073
Sabeel Ansari <sabeel.ansari@intel.com>
2024-07-12 10:13:23 -0700

    Add step0 and target_copy
    Signed-off-by: Sabeel Ansari <sabeel.ansari@intel.com>

---
+++ 0-setup-imc_acc.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+# Define variables
+IMC="root@100.0.0.100"
+ACC="root@192.168.0.2"
+HOST="root@10.166.232.1"
+
+LOCAL_ARTIFACTS_FOLDER="./target_copy/lnp"
+LOCAL_PKG_FILE="./target_copy/lnp/fxp-net_linux-networking.pkg"
+LOCAL_CUSTOM_LOAD_FILE="./target_copy/load_custom_pkg.sh"
+LOCAL_ES2K_SKIP_P4_FILE="./target_copy/es2k_skip_p4.conf"
+
+REMOTE_PATH1_IMC="/work/scripts/"
+REMOTE_FILES_TO_DELETE_IMC=("fxp-net_linux-networking.pkg" "load_custom_pkg.sh")  # Files to delete on IMC
+REMOTE_PATH1_ACC="/usr/share/stratum/"
+REMOTE_ACC_PKG_NAME="lnp"
+
+# SSH options to suppress warnings and errors
+SSH_OPTIONS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR"
+
+
+# Run command on host
+echo "Stopping IDPF on host..."
+ssh $SSH_OPTIONS $HOST "rmmod idpf"
+
+
+# Function to check if a machine is up
+check_machine_up() {
+    local machine=$1
+    local max_attempts=30
+    local attempt=1
+
+    echo "Waiting for $machine to come up..."
+    while [ $attempt -le $max_attempts ]; do
+        if ssh $SSH_OPTIONS $machine "exit" &>/dev/null; then
+            echo "$machine is up!"
+            return 0
+        fi
+        echo "Attempt $attempt: $machine not yet accessible. Waiting..."
+        sleep 10
+        ((attempt++))
+    done
+    echo "Error: $machine did not come up after $max_attempts attempts."
+    return 1
+}
+
+# Delete old .pkg files on IMC
+echo "Deleting old .pkg files on IMC..."
+for file in "${REMOTE_FILES_TO_DELETE_IMC[@]}"
+do
+    ssh $SSH_OPTIONS $IMC "rm -f $REMOTE_PATH1_IMC/$file"
+done
+
+# Delete old files on ACC
+echo "Deleting old artifacts on ACC..."
+ssh $SSH_OPTIONS $IMC "ssh $SSH_OPTIONS $ACC 'rm -rf $REMOTE_PATH1_ACC/$REMOTE_ACC_PKG_NAME'"
+
+# Copy files to IMC
+echo "Copying files to IMC..."
+scp $SSH_OPTIONS $LOCAL_PKG_FILE $LOCAL_CUSTOM_LOAD_FILE $IMC:$REMOTE_PATH1_IMC
+
+# Reboot IMC (which will also reboot ACC)
+echo "Rebooting IMC..."
+ssh $SSH_OPTIONS $IMC "reboot"
+
+sleep 2
+
+# Wait for IMC to come up
+if ! check_machine_up $IMC; then
+    echo "Failed to connect to IMC. Exiting."
+    exit 1
+fi
+
+# Verify values in host config
+echo "Checking for specific values in IMC..."
+if ssh $SSH_OPTIONS $IMC "
+    [ -L /etc/dpcp/package/default_pkg.pkg ] && [ \$(readlink /etc/dpcp/package/default_pkg.pkg) = '/etc/dpcp/package/fxp-net_linux-networking.pkg' ] &&
+    grep -q 'sem_num_pages = 28' /etc/dpcp/cfg/default_cp_init.cfg &&
+    grep -q 'lem_num_pages = 10' /etc/dpcp/cfg/default_cp_init.cfg &&
+    grep -q 'mod_num_pages = 2' /etc/dpcp/cfg/default_cp_init.cfg &&
+    grep -q 'acc_apf = 16' /etc/dpcp/cfg/default_cp_init.cfg &&
+    grep -q 'cpf_host = 4' /etc/dpcp/cfg/default_cp_init.cfg &&
+    grep -qP 'comm_vports\s*=\s*\(\(\[5,0\],\[4,0\]\),\(\[0,3\],\[4,3\]\)\)' /etc/dpcp/cfg/default_cp_init.cfg
+"; then
+    echo "Node config verified on IMC!"
+else
+    echo "Error: Node config required values not found in IMC. Exiting."
+    exit 1
+fi
+
+# Wait for ACC to come up
+echo "Waiting for ACC to come up..."
+attempt=1
+while [ $attempt -le 30 ]; do
+    if ssh $SSH_OPTIONS $IMC "ssh $SSH_OPTIONS $ACC 'exit'" &>/dev/null; then
+        echo "ACC is up!"
+        break
+    fi
+    echo "Attempt $attempt: ACC not yet accessible. Waiting..."
+    sleep 10
+    ((attempt++))
+done
+
+if [ $attempt -gt 30 ]; then
+    echo "Error: ACC did not come up after 30 attempts."
+    exit 1
+fi
+
+# Copy files to ACC through IMC
+echo "Copying artifacts folder to ACC..."
+scp $SSH_OPTIONS -r -o ProxyCommand="ssh $SSH_OPTIONS -W %h:%p $IMC" $LOCAL_ARTIFACTS_FOLDER $ACC:$REMOTE_PATH1_ACC
+echo "Copying es2k_skip_p4.conf file to ACC..."
+scp $SSH_OPTIONS -o ProxyCommand="ssh $SSH_OPTIONS -W %h:%p $IMC" $LOCAL_ES2K_SKIP_P4_FILE $ACC:$REMOTE_PATH1_ACC/es2k/
+
+# first time, gen certs and untar p4.tar.gz
+# Run a command on ACC
+#echo "Running command on ACC..."
+#ssh $SSH_OPTIONS -o ProxyCommand="ssh $SSH_OPTIONS -W %h:%p $IMC" $ACC "your_command_here"
+
+# Sync time
+echo
+echo "Sync'ing time..."
+./sync_time.sh
+
+echo "Setup complete!"
---
+++ target_copy/load_custom_pkg.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+CP_INIT_CFG=/etc/dpcp/cfg/cp_init.cfg
+echo "Checking for custom package..."
+if [ -e fxp-net_linux-networking.pkg ]; then
+    echo "Custom package fxp-net_linux-networking.pkg found. Overriding default package"
+    cp  fxp-net_linux-networking.pkg /etc/dpcp/package/
+    rm -rf /etc/dpcp/package/default_pkg.pkg
+    ln -s /etc/dpcp/package/fxp-net_linux-networking.pkg /etc/dpcp/package/default_pkg.pkg
+   sed -i 's/sem_num_pages = .*;/sem_num_pages = 28;/g' $CP_INIT_CFG
+   sed -i 's/lem_num_pages = .*;/lem_num_pages = 10;/g' $CP_INIT_CFG
+   sed -i 's/mod_num_pages = .*;/mod_num_pages = 2;/g' $CP_INIT_CFG
+   sed -i 's/acc_apf = 4;/acc_apf = 16;/g' $CP_INIT_CFG
+   sed -i 's/comm_vports = .*/comm_vports = (([5,0],[4,0]),([0,3],[4,3]));/g' $CP_INIT_CFG
+else
+    echo "No custom package found. Continuing with default package"
+fi
+
