40d47126f80cf9ded79740c716bfa08e2f637664
Sabeel Ansari <sabeel.ansari@intel.com>
2025-01-17 11:32:19 -0800

    Update verify_es2k_skip
    Signed-off-by: Sabeel Ansari <sabeel.ansari@intel.com>

--- verify_es2k_skip_file.sh
+++ verify_es2k_skip_file.sh
@@ -3,6 +3,37 @@
 # Source the environment file
 source ./config.env
 
+# List of interfaces to check
+interfaces=(
+    enp0s1f0d1 enp0s1f0d2 enp0s1f0d3 enp0s1f0d4 enp0s1f0d5
+    enp0s1f0d6 enp0s1f0d7 enp0s1f0d8 enp0s1f0d9 enp0s1f0d10
+    enp0s1f0d11 enp0s1f0d12 enp0s1f0d13 enp0s1f0d14 enp0s1f0d15
+)
+
+# Function to check if an interface is up
+check_interface() {
+    local interface=$1
+    ssh $SSH_OPTIONS $IMC "ssh $SSH_OPTIONS $ACC 'ip link show $interface | grep -q \"state UP\"'"
+    return $?
+}
+
+# Wait for all interfaces to be up
+while true; do
+    all_up=true
+    for interface in "${interfaces[@]}"; do
+        if ! check_interface "$interface"; then
+            all_up=false
+            printf "\nWaiting for $interface to be up on ACC..."
+            break
+        fi
+    done
+    if $all_up; then
+        printf "\nAll interfaces on ACC are up!\n"
+        break
+    fi
+    sleep 3
+done
+
 # Verify MAC addresses on ACC
 printf "Verifying MAC addresses on ACC..."
 
